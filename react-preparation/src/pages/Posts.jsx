1e1f55bdbfb8799ea323d45f4c30c404f8342600
Noah <azsnobird1227@yahoo.com>
2024-06-06 15:22:31 -0500

    pages made, basic structure for getting posts

---
+++ react-preparation/src/pages/Posts.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const Posts = () => {
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+      setPosts(data)
+      console.log(data)
+    }
+    fetchPosts();
+  }, []);
+
+   
+
+  return (
+    <div> 
+      {posts.map(post => <div> {post.id} </div>)}
+    </div>
+  )
+}
+
+export default Posts;
